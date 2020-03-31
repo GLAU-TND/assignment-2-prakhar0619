084dcd8d2ce1c82d5975a8545c982e90e760d020
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-31 19:09:06 +0530

    my main

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -5,6 +5,32 @@
  *  Time: 7:06 PM
  */
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
+import java.util.ArrayList;
+
 //executable class
 public class MyMain {
+    public static void main(String[] args) {
+        ArrayList<Student> studentList = new ArrayList<Student>();
+
+        studentList.add(new Student(1, "Manish", 5, 4));
+        studentList.add(new Student(2, "Aman", 0, 0));
+        studentList.add(new Student(3, "Prakhar", 1, 1));
+        studentList.add(new Student(4, "Tina", 6, 3));
+        studentList.add(new Student(5, "Dharmendra", 0, 0));
+        studentList.add(new Student(6, "Rahul", 4, 2));
+        studentList.add(new Student(7, "Yoyo", 2, 1));
+        studentList.add(new Student(8, "ishi", 1, 1));
+        studentList.add(new Student(9, "Unnati", 0, 0));
+        studentList.add(new Student(10, "shivam", 5, 0));
+        studentList.add(new Student(11, "Suraj", 0, 0));
+
+        MyCircularQueue ob = new MyCircularQueue();
+        ob.enqueue(studentList);
+
+        ob.printQueue();
+    }
 }
