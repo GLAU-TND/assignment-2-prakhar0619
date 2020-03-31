929da3753ed2ff352f0799b34caa8535e7b73017
prakhar agarwal <prakhar.agarwal1_cs18@gla.ac.in>
2020-03-31 19:01:25 +0530

    my main

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,32 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree MyBinarySearchTree =new MyBinarySearchTree();
+        MyBinarySearchTree.add(50);
+        MyBinarySearchTree.add(95);
+        MyBinarySearchTree.add(560);
+        MyBinarySearchTree.add(356);
+        MyBinarySearchTree.add(820);
+        MyBinarySearchTree.add(404);
+        MyBinarySearchTree.add(200);
+        MyBinarySearchTree.add(78);
+        MyBinarySearchTree.add(100);
+        MyBinarySearchTree.add(60);
+        MyBinarySearchTree.add(69);
+        MyBinarySearchTree.add(85);
+        MyBinarySearchTree.add(72);
+        MyBinarySearchTree.add(50);
+        MyBinarySearchTree.add(12);
+        MyQueue queue = new MyQueue();
+        queue.enqueue(MyBinarySearchTree.root);
+        queue.printQueue();
+    }
+
 }
